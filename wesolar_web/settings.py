7a0f67a6840f80019655d8d65708ac848c889b4e
phaniuddagiri <phanindrauddagiri05@gmail.com>
2026-03-28 21:48:31 +0530

    updated

--- wesolar_web/settings.py
+++ wesolar_web/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'wesolar_web.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wesolar_db',
+        'USER': 'root',
+        'PASSWORD': 'PhaniUddagiri@2005',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
